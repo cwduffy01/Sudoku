787beee1fd62941f0bce06a3f59708ef1fc41b27
Carson Duffy <cwduffy01@gmail.com>
2020-11-12 09:29:23 -0600

    updates

---
+++ src/Solver.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Solver {
+
+    private Sudoku sudoku;
+
+    public Solver(Sudoku sudoku) {
+        this.sudoku = sudoku;
+    }
+
+    public void scan(int i, int j){
+        int[] row = sudoku.getRow(i);
+        int[] col = sudoku.getCol(j);
+        Cell cell = sudoku.getCell(i, j);
+        for (int k = 0; k < row.length; k++) {
+            cell.removeAnswer(row[k]);
+            cell.removeAnswer(col[k]);
+        }
+        sudoku.update();
+    }
+}
